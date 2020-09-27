d687c3e1146038cc82d285dd2b5c2df60932fe28
galuwka <galinasale@gmail.com>
2020-09-27 19:20:39 +0300

    Clean-up

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const http = require('http');
 const config = require('./config');
 const logger = require('./logger');
 
-const env = config.ENV;
-const port = config.PORT;
+const { ENV, PORT } = config;
 
 http.createServer((req, res) => {
     logger.log('New incoming request');
     res.writeHeader(200, {'Content-Type': 'application/json'});
     res.end(JSON.stringify({message: 'Hello world!'}));
-}).listen(port, () => {
-    logger.log(`Current environment: ${env}`);
-    logger.log(`Listening on port ${port}`);
+}).listen(PORT, () => {
+    logger.log(`Current environment: ${ENV}`);
+    logger.log(`Listening on port ${PORT}`);
 });
